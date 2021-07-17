46f4d7f9ca378c938be5dcf7a5082a8b8a31ec7c
fawaz-strawberry <fawazgames223@gmail.com>
2021-07-17 11:06:47 -0700

    Modified getMouseAdjustment Script

--- PythonScripts/getMouseAdjustments.py
+++ PythonScripts/getMouseAdjustments.py
@@ -1,8 +1,18 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+            
 import json
 import math
 from flask import Flask, request, jsonify
 from werkzeug.datastructures import MultiDict
 import logging
+
+
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -10,7 +20,24 @@ app = Flask(__name__)
 
 calib_x = 0
 current_x = 0
-CIRC = 1516
+CIRC = 3000
+
+class MouseClient():
+	def __init__(self):
+		super().__init__()
+		self.state = [0, 0, 0, 0]
+		self.bus = dbus.SystemBus()
+		self.btkservice = self.bus.get_object(
+			'org.thanhle.btkbservice', '/org/thanhle/btkbservice')
+		self.iface = dbus.Interface(self.btkservice, 'org.thanhle.btkbservice')
+	def send_current(self):
+		try:
+			self.iface.send_mouse(0, bytes(self.state))
+		except OSError as err:
+			error(err)
+
+client = MouseClient()
+
 
 @app.route('/')
 def index():
@@ -36,6 +63,7 @@ def mousePos():
     #calib_x is front position
     #current_x is current position
     global current_x
+    global client
     #x is the target position
     
     #check if moving to the right or left is faster
@@ -47,43 +75,75 @@ def mousePos():
 
     if((midpoint - x < .5 and midpoint - x > 0) or (x - current_x < .5 and x - current_x > 0)):
         #move right
-        print("Right")
+        #print("Right")
         diff = x - current_x
         if(diff < 0):
             diff = 1 + diff
         
-        dx = CIRC * diff
+        #print(diff)
+        
+        #dx = CIRC * diff
+
+        client.state[0] = int(0)
+        client.state[2] = int(0)
+        client.state[3] = int(0)
+        client.state[1] = 3 * int((diff * 127))
+        print("Right: " + str(3 * int(diff * 127)))
+        client.send_current()
+        #print("Right: " + str(dx))
 
         #print("x: " + str(x) + " cur x: " + str(current_x))
-        while dx > 0:
-           
-            if(dx > 127):
-                #print("Moving R 127")
-                dx -= 127
-                #move right 127
-            else:
-                #print("Moving R " + str(dx))
-                dx -= dx
-                #move right dx
+#        while dx > 0:
+#           
+#            if(dx >= 127):
+#                print("Moving R 127")
+#                dx -= 127
+#                #move right 127
+#                
+#                client.state[1] = int(127)
+#                client.send_current()
+#            else:
+#                print("Moving R " + str(int(dx)))
+#                dx -= dx
+#                client.state[1] = int(dx)
+#                client.send_current()
+#               #move right dx
         
     else:
         #move left
-        print("Left")
+        #print("Left")
         diff = current_x - x
         if(diff < 0):
             diff = 1 + diff
         
+        #print(diff)
+        
         dx = CIRC * diff
 
-        while dx > 0:
-            if(dx > 127):
-                #print("Moving L 127")
-                dx -= 127
-                #move 129
-            else:
-                #print("Moving L " + str(dx))
-                dx -= dx
-                #move 256 - dx
+        client.state[0] = int(0)
+        client.state[2] = int(0)
+        client.state[3] = int(0)
+        client.state[1] = int(256 - 3 * (diff * 127))
+        print("Left: " + str(int(256 - 3 * (diff * 127))))
+        if(client.state[1] == 256):
+            client.state[1] = 128
+        client.send_current()
+#        while dx > 0:
+#            if(dx >= 127):
+#                print("Moving L 127")
+#                dx -= 127
+#                client.state[1] = int(129)
+#                client.send_current()
+#                #move 129
+#            else:
+#                print("Moving L " + str(int(256 - dx)))
+#                dx -= dx
+#                client.state[1] = int(256 - dx)
+#                if(client.state[1] == 256):
+#                    client.state[1] = 128
+#                #print(client.state)
+#                client.send_current()
+#                #move 256 - dx
 
     current_x = x
 
@@ -105,9 +165,7 @@ def calibrate():
 
     current_x = calib_x
 
-    #print(current_x)
-
     return jsonify({"status":"huh"})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
