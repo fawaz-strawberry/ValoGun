ff00fecb83e926a9c6d65b096c17a6f8a34bacbc
Fawaz <fawazgames223@gmail.com>
2021-07-19 20:27:15 -0700

    Simplified everything a ton

--- PythonScripts/pose_collector.py
+++ PythonScripts/pose_collector.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QTimer
 from PyQt5.QtGui import QIcon, QFont
 
 class App(QMainWindow):
@@ -13,36 +13,56 @@ class App(QMainWindow):
         self.width = 1920
         self.height = 1080
         self.initUI()
+        self.i = 0
+        self.poses = ["crouch", "stand", "jump"]
+
+    @pyqtSlot()
+    def update_label(self):
+        print("Updating")
+        self.labelA.setText(self.poses[self.i])
+        self.i += 1
+        if(self.i >= len(self.poses)):
+            self.i = 0
+        self.labelA.setFont(QFont('Times', 100))
+
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.statusBar().showMessage('Message in statusbar.')
 
-        # button = QPushButton('PyQt5 button', self)
-        # button.setGeometry(100, 70, 200, 200)
-        # button.setToolTip("This is the best button")
-        # button.clicked.connect(self.on_click)
 
-        labelA = QLabel("Times font", self)
-        labelA.setText('Crouch')
-        labelA.setGeometry(100, 70, 1800, 1000)
-        labelA.setFont(QFont('Times', 100))
+        self.labelA = QLabel("Arial", self)
+        self.labelA.setGeometry(100, 70, 1800, 1000)
+
+        button = QPushButton('Start Collect', self)
+        button.setGeometry(self.width / 2 - 200, 70, 200, 200)
+        button.setToolTip("Begin Data Collection")
+        button.clicked.connect(self.on_click)
+
+        timer = QTimer()
+        timer.timeout.connect(self.update_label)
+        timer.start(1000)
+
 
         self.show()
 
 
-    @pyqtSlot()
-    def on_click(self):
-        print('PyQt5 button click')
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
 
+
 #Create list of different poses
-poses = ["crouch", "stand", "jump"]
+
 
 #For each pose, ask user to do pose and then save video from camera
 #with label of the pose. Connect with phone app to pull the video
